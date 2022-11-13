6dcdecf6f15e260cb75ffd5524d0098a090a372d
Andrey Shumila <Andrii_Shumila@epam.com>
2022-11-13 22:01:20 +0200

    Feature-Components-part-2: Sorting markup was fixed

--- src/components/Sorting/Sorting.tsx
+++ src/components/Sorting/Sorting.tsx
@@ -15,20 +15,22 @@ export default function Sorting({ sortingChange, sort }: SortingProps) {
     <div className='sorting'>
       <label htmlFor='sorting-dropdown' className='sorting-text sorting-label'>sort by</label>
 
-      <Select
-        className='sorting-text sorting-dropdown'
-        value={sort}
-        onChange={handleChange}
-        displayEmpty
-        inputProps={{ 'aria-label': 'Without label', 'classes' : 'sorting-text sorting-dropdown' }}
-      >
-        <MenuItem className='test' value={'title'}>Title</MenuItem>
-        <MenuItem value={'releaseDate'}>Release date</MenuItem>
-        <MenuItem value={'rating'}>Rating</MenuItem>
-        <MenuItem value={'genre'}>Genre</MenuItem>
-        <MenuItem value={'runtime'}>Runtime</MenuItem>
-        <MenuItem value={'overview'}>Overview</MenuItem>
-      </Select>
+      <div className='sorting-dropdown'>
+        <Select
+          className='sorting-text '
+          value={sort}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label', 'classes' : 'sorting-text sorting-dropdown' }}
+        >
+          <MenuItem className='test' value={'title'}>Title</MenuItem>
+          <MenuItem value={'releaseDate'}>Release date</MenuItem>
+          <MenuItem value={'rating'}>Rating</MenuItem>
+          <MenuItem value={'genre'}>Genre</MenuItem>
+          <MenuItem value={'runtime'}>Runtime</MenuItem>
+          <MenuItem value={'overview'}>Overview</MenuItem>
+        </Select>
+      </div>
     </div>
   );
 }
